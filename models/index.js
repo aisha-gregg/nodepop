6e8c9815b384e5b87807948e7c442ac1556a1269
aisha-gregg <aishagregg@gmail.com>
2019-07-29 19:23:50 +0200

    drop collection

--- models/index.js
+++ models/index.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/nodepop", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost:27017/nodepop", {
+  useNewUrlParser: true
+});
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function() {
+  mongoose.connection.db.dropCollection("tags");
+
   const tagSchema = new mongoose.Schema({
     id: Number,
     name: String
@@ -12,8 +16,24 @@ db.once("open", function() {
     id: 1,
     name: "Work"
   });
-  console.log(work.name);
 
-  work.save()
+  const lifestyle = new Tag({
+    id: 2,
+    name: "Lifestyle"
+  });
+
+  const motor = new Tag({
+    id: 3,
+    name: "Motor"
+  });
+
+  const mobile = new Tag({
+    id: 4,
+    name: "Mobile"
+  });
+
+  work.save();
+  lifestyle.save();
+  motor.save();
+  mobile.save();
 });
-
