966e7a6ab5fb3525f84d9de09a218b734a7e974e
Aisha Gregg <aishagregg@gmail.com>
2019-08-20 23:59:02 +0200

    add api functionality

--- public/scripts/filters.js
+++ public/scripts/filters.js
@@ -1,5 +1,18 @@
-const workFilter = document.querySelector("#filterSelect");
-workFilter.addEventListener("click", async function() {
-  const results = await Ad.find({}).then();
-  alert(results);
+const motorFilter = document.querySelector("#motor");
+motorFilter.addEventListener("click", async function() {
+  const response = await fetch("http://localhost:3000/ads?tag=Motor");
+  const ads = await response.json();
+
+  const mobileAds = document.querySelector(".mobile-ads");
+  mobileAds.innerHTML = ads.map(function(ad) {
+    return `
+      <div class="product-info">
+        <img src="/images/galaxy s6.jpg" alt="" />
+        <span>${ad.article.name}</span>
+        <span><%= ad.article.id %></span>
+        <span><%= ad.id %></span>
+        <span><%= ad.article.description %></span>
+      </div>
+    `;
+  });
 });
